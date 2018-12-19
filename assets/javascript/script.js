c5d792ca1c2cd410471bc4dbb23b9faec38e7d9f
johnsonchon <thejohnson.does.it@gmail.com>
2018-12-18 21:30:04 -0800

    johnson new-end of class

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,25 @@
-var location = sessionStorage.getItem("location")
+$(document).ready(function () {
 
-// Outcome will give name of city
-var APIKey = "4282776-27fa57906aa34123504a6dc56";
-var city = "new york";
-var queryURL = "https://pixabay.com/api/?key=" + APIKey + "&q=" + city + "&image_type=photo" ;
+    var location = sessionStorage.getItem("location")
+
+    // Outcome will give name of city
+
+    function cityImg() {
+        var APIKey = "4282776-27fa57906aa34123504a6dc56";
+        var city = "new york";
+        var queryURL = "https://pixabay.com/api/?key=" + APIKey + "&q=" + city + "&image_type=photo";
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    // console.log(response.hits[0].largeImageURL);
-    var img = $("<img>");
-    img.attr("src",response.hits[0].largeImageURL);
-    $(".text-muted").html(img);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            // console.log(response.hits[0].largeImageURL);
+            var img = $("<img>");
+            img.attr("src", response.hits[0].largeImageURL);
+            $(".text-muted").html(img);
+        });
+    }
+
+
 });
-
